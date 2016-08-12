bdcdaa52f166cab33b6650d04d7909f1996755a5
Jared Ballou <github@jballou.com>
2016-08-12 16:10:25 -0400

    Saving changes since I keep losing power

--- utilities/gamedata/__init__.py
+++ utilities/gamedata/__init__.py
@@ -1,28 +1,49 @@
 import vpk
 import os
 import sys
+import yaml
 import configparser
 from pprint import pprint
 from glob import glob
 from fnmatch import fnmatch
 
-extract_paths = {
-	"maps/": "*.txt",
-	"materials/overviews/": "*",
-	"materials/vgui/gameui/": "*",
-	"materials/vgui/hud/": "*",
-	"materials/vgui/hud_doi/": "*",
-	"materials/vgui/inventory/": "*",
-	"materials/vgui/logos/": "*",
-	"materials/vgui/maps/": "*",
-	"resource/": "*.txt",
-	"resource/": "*.res",
-	"scripts/playlists/": "*.txt",
-	"scripts/theaters/": "*.theater",
-}
-
 class GameData(object):
-	def __init__(self, game_name, game_root, extract_root, vpk_files=["misc","materials"]):
+	def __init__(self, config_file = None, extract_root=None):
+		self.games = {}
+		self.load_config_file(config_file)
+		self.set_extract_root(extract_root)
+		self.load_games()
+
+	def set_extract_root(self,extract_root=None,update_games=False):
+		if extract_root is None:
+			extract_root = self.config['extract_root']
+		self.extract_root = extract_root
+		if update_games:
+			for name in self.games.keys():
+				self.games[name].extract_root = extract_root
+
+	def load_games(self):
+		for name,data in self.config['games'].iteritems():
+			if not 'game_name' in data.keys():
+				data['game_name'] = name
+			if not 'vpk_files' in data.keys():
+				data['vpk_files'] = self.config['vpk_files']
+			if not 'extract_root' in data.keys():
+				data['extract_root'] = self.extract_root
+			data['parent'] = self
+			self.games[name] = Game(**data)
+
+	def load_config_file(self,config_file=None):
+		if config_file is None:
+			config_file = os.path.join(os.path.dirname(__file__),"config.yaml")
+		if os.path.isfile(config_file):
+			with open(config_file, 'r') as ymlfile:
+				self.config = yaml.load(ymlfile)
+				self.config_file = config_file
+
+class Game(object):
+	def __init__(self, game_name, game_root, extract_root, vpk_files, parent):
+		self.parent = parent
 		self.extract_root = extract_root
 		self.game_name = game_name
 		self.vpk_files = vpk_files
@@ -31,6 +52,7 @@ class GameData(object):
 		self.steam_inf = self.load_steam_inf()
 		self.load_vpks()
 		self.extract_vpk_files(force=False)
+		pprint(self.config)
 
 	def load_steam_inf(self, game_name=None, game_root=None, inf_file="steam.inf"):
 		if game_name is None:
@@ -54,7 +76,7 @@ class GameData(object):
 		if game_root is None:
 			game_root = self.game_root
 		for vpk_file in vpk_files:
-			self.vpks[vpk_file] = VPKFile(vpk_file = os.path.join(game_root,"%s_%s_dir.vpk" % (game_name, vpk_file)))
+			self.vpks[vpk_file] = VPKFile(vpk_file = os.path.join(game_root,"%s_%s_dir.vpk" % (game_name, vpk_file)), parent=self)
 			#self.vpks[vpk_file].get_file_list()
 
 	def extract_vpk_files(self, extract_root = None, game_name=None, force=False):
@@ -79,7 +101,8 @@ class GameData(object):
 		return False
 
 class VPKFile(object):
-	def __init__(self,vpk_file):
+	def __init__(self,vpk_file,parent):
+		self.parent = parent
 		self.vpk_obj = vpk.open(vpk_file)
 
 	def get_file_list(self):
@@ -97,4 +120,6 @@ class VPKFile(object):
 		print "Extracting %s to %s" % (os.path.basename(extract_path),extract_dir)
 
 if __name__ == "__main__":
-	gd = GameData(game_name="doi", vpk_files=["misc","materials"], game_root="/home/doiserver/serverfiles/doi", extract_root = "/home/insserver/insurgency-tools/data/mods")
+	gd = GameData(extract_root = "/home/insserver/insurgency-tools/data/mods")
+
+#game_name="doi", vpk_files=["misc","materials"], game_root="/home/doiserver/serverfiles/doi", 
